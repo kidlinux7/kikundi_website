98862a6f742d33bb728f4fd9ca4e2f2cdb7fbfa1
kidlinux7 <princeherman08@gmail.com>
2023-01-12 10:14:02 +0300

    Email Subscribers

--- pages/models.py
+++ pages/models.py
@@ -11,3 +11,11 @@ class Contact_us(models.Model):
 
     def __str__(self):
         return self.email
+
+class Subscriber(models.Model):
+    email = models.EmailField(max_length=254,null=False,blank=False)
+    date_created = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.email
+
--- pages/views.py
+++ pages/views.py
@@ -8,15 +8,25 @@ from django.template import RequestContext
 
 def home(request):
     if request.method == 'POST':
-        name = request.POST['name']
-        email = request.POST['email']
-        phone_number = request.POST['phone_number']
-        subject = request.POST['subject']
-        message = request.POST['message']
+        if 'contactUsButton' in request.POST:
+            name = request.POST['name']
+            email = request.POST['email']
+            phone_number = request.POST['phone_number']
+            subject = request.POST['subject']
+            message = request.POST['message']
 
-        contact_us = Contact_us.objects.create(name = name,email=email,phone_number=phone_number,subject=subject,message=message)
-        contact_us.save()
-        messages.info(request,'Thank you, your information has been received')
+            contact_us = Contact_us.objects.create(name = name,email=email,phone_number=phone_number,subject=subject,message=message)
+            contact_us.save()
+            messages.info(request,'Thank you, your information has been received')
+
+        if 'subscriberEmailButton' in request.POST:
+            email = request.POST['subscriberEmail']
+            our_subscriber = Subscriber.objects.create(email=email)
+            our_subscriber.save()
+            messages.info(request,'Thank you, your information has been received')
+        return redirect('home')
+
+
         return redirect('home')
     else:
         context = {}
